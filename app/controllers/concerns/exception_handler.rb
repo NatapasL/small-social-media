9e25ba59ea4e4453a8ba8f4f5b0137466cd5ce73
NatapasL <natapas.lue@gmail.com>
2024-08-08 23:55:30 +0700

    Update exception handler message

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,27 +5,35 @@ module ExceptionHandler
 
   included do
     rescue_from(Exceptions::Unauthorized) do
-      render json: { errors: 'Unauthorized' }, status: :unauthorized
+      render json: { errors: 'Unauthorized.' }, status: :unauthorized
     end
 
     rescue_from(Exceptions::UnprocessableEntity) do |exception|
-      render json: { errors: 'Unprocessable', message: exception }, status: :unprocessable_entity
+      render json: { errors: 'Unprocessable.', message: exception }, status: :unprocessable_entity
     end
 
     rescue_from(Exceptions::BadRequest) do |exception|
-      render json: { errors: "There's something wrong with your request", message: exception }, status: :bad_request
+      render json: { errors: "There's something wrong with your request.", message: exception }, status: :bad_request
     end
 
     rescue_from(Exceptions::Forbidden) do
-      render json: { errors: 'You are not allowed' }, status: :forbidden
+      render json: { errors: 'You are not allowed.' }, status: :forbidden
     end
 
     rescue_from(ActiveRecord::RecordNotFound) do
-      render json: { errors: 'Record not found' }, status: :not_found
+      render json: { errors: 'Record not found.' }, status: :not_found
     end
 
     rescue_from(Exceptions::RouteNotFound) do
-      render json: { errors: 'Route not found' }, status: :not_found
+      render json: { errors: 'Route not found.' }, status: :not_found
+    end
+
+    rescue_from(ActiveRecord::RecordInvalid) do |exception|
+      render json: { errors: "Can't save record.", message: exception }, status: :unprocessable_entity
+    end
+
+    rescue_from(ActionController::ParameterMissing) do |exception|
+      render json: { errors: 'Parameter missing.', message: exception }, status: :bad_request
     end
   end
 end
